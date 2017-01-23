8a47d7564778fad72c2f8ffe97a8c2a77cc601a0
raguirre <raguirre@blastasia.com>
2017-01-23 20:58:09 +0800

    added two sorted arrays

---
+++ TestSuite.CrackingTheCode.ReadThrough.Test/TechnicalQuestions/TwoSortedArraysTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using TestSuite.CrackingTheCode.ReadThrough.TechnicalQuestions;
+
+namespace TestSuite.CrackingTheCode.ReadThrough.Test.TechnicalQuestions
+{
+    [TestClass]
+    public class TwoSortedArraysTest
+    {
+        private TwoSortedArrays sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sut = new TwoSortedArrays();
+        }
+
+        [TestMethod]
+        public void TestBruteForce()
+        {
+            // Arrange
+            var a = new int[] { 13, 27, 35, 40, 49, 55, 59 };
+            var b = new int[] { 17, 35, 39, 40, 55, 58, 60 };
+
+            // Act
+            var result = sut.BruteForce(a, b);
+
+            // Assert
+            result[0].ShouldEqual(35);
+            result[1].ShouldEqual(40);
+            result[2].ShouldEqual(55);
+
+            // Analyze
+            sut.Counter.Print(a.Length * b.Length);
+        }
+
+        [TestMethod]
+        public void TestHashSet()
+        {
+            // Arrange
+            var a = new int[] { 13, 27, 35, 40, 49, 55, 59 };
+            var b = new int[] { 17, 35, 39, 40, 55, 58, 60 };
+
+            // Act
+            var result = sut.HashSet(a, b);
+
+            // Assert
+            result[0].ShouldEqual(35);
+            result[1].ShouldEqual(40);
+            result[2].ShouldEqual(55);
+
+            // Analyze
+            sut.Counter.Print(b.Length);
+        }
+
+        [TestMethod]
+        public void TestOptimized()
+        {
+            // Arrange
+            var a = new int[] { 13, 27, 35, 40, 49, 55, 59 };
+            var b = new int[] { 17, 35, 39, 40, 55, 58, 60 };
+
+            // Act
+            var result = sut.Optimized(a, b);
+
+            // Assert
+            result[0].ShouldEqual(35);
+            result[1].ShouldEqual(40);
+            result[2].ShouldEqual(55);
+
+            // Analyze
+            sut.Counter.Print(b.Length);
+        }
+    }
+}
---
+++ TestSuite.CrackingTheCode.ReadThrough/TechnicalQuestions/TwoSortedArrays.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using TestSuite.CrackingTheCode.ReadThrough.Utils;
+
+namespace TestSuite.CrackingTheCode.ReadThrough.TechnicalQuestions
+{
+    /// <summary>
+    /// Given two sorted arrays, find the number of elements in common. The arrays are the same length
+    /// and each has all distinct elements.
+    /// A: 13 27 35 40 49 55 59
+    /// B: 17 35 39 40 55 58 60
+    /// </summary>
+    public class TwoSortedArrays
+    {
+        public BigOCounter Counter { get; set; } = new BigOCounter();
+
+        public int[] BruteForce(int[] a, int[] b)
+        {
+            var result = new List<int>();
+
+            foreach(var n1 in a)
+            {
+                foreach(var n2 in b)
+                {
+                    this.Counter.Increment();
+
+                    if(n1 == n2)
+                    {
+                        result.Add(n1);
+                        break;
+                    }
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        public int[] HashSet(int[] a, int[] b)
+        {
+            var result = new List<int>();
+
+            var set = new HashSet<int>(a);
+            foreach(var n in b)
+            {
+                this.Counter.Increment();
+
+                if (set.Contains(n))
+                    result.Add(n);
+            }
+
+            return result.ToArray();
+        }
+
+        public int[] Optimized(int[] a, int[] b)
+        {
+            var result = new List<int>();
+
+            int j = 0;
+            foreach(var n1 in a)
+            {
+                this.Counter.Increment();
+
+                var n2 = b[j];
+                while(n2 < n1)
+                {
+                    this.Counter.Increment();
+
+                    j++;
+                    n2 = b[j];
+                }
+
+                if (n1 == n2)
+                    result.Add(n1);
+            }
+
+            return result.ToArray();
+        }
+    }
+}

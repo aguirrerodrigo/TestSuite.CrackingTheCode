b80090c02867fb916e8425b2ff34150a1024bdea
raguirre <raguirre@blastasia.com>
2017-02-05 00:48:19 +0800

    added string rotation

---
+++ TestSuite.CrackingTheCode.ReadThrough.Test/InterviewQuestions/StringRotationTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using TestSuite.CrackingTheCode.ReadThrough.InterviewQuestions;
+
+namespace TestSuite.CrackingTheCode.ReadThrough.Test.InterviewQuestions
+{
+    [TestClass]
+    public class StringRotationTest
+    {
+        private StringRotation sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sut = new StringRotation();
+        }
+
+        [TestMethod]
+        public void TestIsRotation()
+        {
+            // Arrange
+            var s1 = "waterbottle";
+            var s2 = "erbottlewat";
+
+            // Act
+            var result = sut.IsRotation(s1, s2);
+
+            // Assert
+            result.ShouldBeTrue();
+        }
+    }
+}
---
+++ TestSuite.CrackingTheCode.ReadThrough/InterviewQuestions/StringRotation.cs
@@ -0,0 +1,16 @@
+﻿namespace TestSuite.CrackingTheCode.ReadThrough.InterviewQuestions
+{
+    /// <summary>
+    /// Assume you have a method isSubst ring which checks if one word is a substring
+    /// of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
+    /// call to isSubstring(e.g., "waterbottle" is a rotation of"erbottlewat").
+    /// </summary>
+    public class StringRotation
+    {
+        public bool IsRotation(string s1, string s2)
+        {
+            var concat = string.Concat(s2, s2);
+            return concat.Contains(s1);
+        }
+    }
+}
